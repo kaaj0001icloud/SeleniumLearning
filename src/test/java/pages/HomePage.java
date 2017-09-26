5df90491edd039734f294d131cc5998577036304
Ajay K Kalangi <kalangi.ajay@gmail.com>
2017-09-27 00:51:27 +0530

    Adding the framework

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage {
+	WebDriver driver;
+
+	
+	@FindBy(xpath = "//*[@id='twotabsearchtextbox']")
+	WebElement searchBox;
+	
+	@FindBy(xpath = "//*[@id='nav-search']/form/div[2]/div/input")
+	WebElement searchButton;
+	
+	@FindBy(xpath = "//*[@id='nav-link-shopall']/span[2]")
+	WebElement shopByCategory;
+	
+	@FindBy(xpath = "//*[@id='nav-flyout-shopAll']/div[2]/span[5]/span")
+	WebElement tvAndAppliances;
+	
+	
+}
